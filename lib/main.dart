8d8326dfd3fda4bc8d18aa59f6b49a02d69fa0bf
Vinicius <viniciusbuenodeoliveira2017@gmail.com>
2025-04-04 10:26:06 -0300

    Criando gerenciamento de estado

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:mvvm/presentation/controllers/theme_controller.dart';
-import 'package:mvvm/presentation/pages/my_home_page.dart';
+import 'package:mvvm/classes/counter_state.dart';
 
 void main() => runApp(const MyApp());
-
-final themeController = ThemeController();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: themeController,
-      builder: (context, value, child) {
-        return MaterialApp(
-          title: 'Flutter Demo',
-          theme: value ? ThemeData.dark() : ThemeData.light(),
-          home: const MyHomePage(),
-        );
-      },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: false),
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final counterState = CounterState();
+  @override
+  void initState() {
+    counterState.addListener(callback);
+    super.initState();
+  }
+
+  void callback() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Gerenciamento de estado")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Valor do estado: ${counterState.counter}"),
+            ElevatedButton(
+              onPressed: () {
+                counterState.increment();
+              },
+              child: Text("Incrementar"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    counterState.removeListener(callback);
+    super.dispose();
   }
 }
