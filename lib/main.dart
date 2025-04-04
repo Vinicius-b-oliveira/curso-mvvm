eaee9df8ff6e005049d7e0170e744e2f10369231
Vinicius <viniciusbuenodeoliveira2017@gmail.com>
2025-04-04 09:08:28 -0300

    Gerenciamento de estado

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm/presentation/controllers/theme_controller.dart';
+import 'package:mvvm/presentation/pages/my_home_page.dart';
+
+void main() => runApp(const MyApp());
+
+final themeController = ThemeController();
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: themeController,
+      builder: (context, value, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: value ? ThemeData.dark() : ThemeData.light(),
+          home: const MyHomePage(),
+        );
+      },
+    );
+  }
+}
